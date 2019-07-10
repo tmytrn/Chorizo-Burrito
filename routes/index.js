fe5fc2755709b1241893b022c4e0fabdf80391d8
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2019-07-10 16:17:20 -0700

    linked store to blog

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
+var request = require('request-promise');
 require('dotenv').config();
 
 var client = new Twitter({
@@ -16,36 +17,65 @@ const cleanTweets = (tweets) => {
   obj.pics = [];
 
   for (i = 0; i < tweets.length; i++) {
-    if (tweets[i].entities.media) {
-      let media = JSON.stringify(tweets[i].entities.media);
-      let img = JSON.parse(media.substring(1, media.length - 1));
-      let imgLink = img.media_url_https
-      let cleanTweet = tweets[i].text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-      if (cleanTweet.includes('—')) {
-        cleanTweet = cleanTweet.replace('—', '');
-        let element = {
-          description: cleanTweet,
-          link: imgLink,
-        }
-        obj.pics.push(element);
+    if (tweets[i].text.includes('—')) {
+      let tweet = tweets[i].text;
+      let cleanText = tweet.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+      cleanText = cleanText.replace('—', '');
+      if (tweets[i].entities.media) {
+        obj.pics.push(picturePost(tweets[i], cleanText))
       }
-    } else {
-      if (tweets[i].entities) {
-        let cleanText = tweets[i].text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-        if (cleanText.includes('—') && !(tweets[i].entities.urls.length == 0)) {
-          cleanText = cleanText.replace('—', '');
-          let url = tweets[i].entities.urls[0].url;
-          let link = {
-            url: url,
-            text: cleanText
-          }
-          obj.links.push(link);
+      else {
+        obj.links.push(linkPost(tweets[i], cleanText))
+      }
 
-        }
-      }
     }
   }
   return obj;
+}
+
+const picturePost = (tweet, cleanText) => {
+  let media = JSON.stringify(tweet.entities.media);
+  let img = JSON.parse(media.substring(1, media.length - 1));
+  let imgLink = img.media_url_https;
+  let element = {
+    description: cleanText,
+    link: imgLink
+  }
+  return element;
+}
+
+const linkPost = (tweet, cleanText) => {
+  if (!tweet.entities.urls.length == 0) {
+    let url = tweet.entities.urls[0].url;
+
+    let element = {
+      url: url,
+      text: cleanText
+    }
+    return element;
+
+  }
+}
+
+const lookUp = (url) => {
+  const lookUpKey = process.env.LINK_PREVIEW_KEY;
+  const site = 'https://api.linkpreview.net';
+  const apiEndPoint = site + '/?key=' + lookUpKey + '&q=' + url;
+
+  let options = {
+    uri: apiEndPoint,
+    json: true
+  }
+
+  return new request(options)
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+
+    })
+
 }
 
 const params = { exclude_replies: true, count: 60 }
@@ -53,18 +83,35 @@ const params = { exclude_replies: true, count: 60 }
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  client.get('statuses/home_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      const obj = cleanTweets(tweets);
-      res.render('index', { title: 'Chorizzo', tweets: obj.links, pictures: obj.pics });
+  client.get('statuses/home_timeline', params)
+    .then((tweets) => {
+      return cleanTweets(tweets);
+    })
+    .then((tweets) => {
+      let pics = tweets.pics;
+      let links = tweets.links;
+      //const posts = Promise.all(links.map(lookUp));
 
-    }
-    else {
-      console.log(error);
-    }
-  });
+      // posts.then((data) => {
+      //   let element = {
+      //     imgDescription: data.description,
+      //     img: data.image,
+      //     url: data.url
+
+      //   }
+      //   return element;
+      // }).then((data) => {
+
+       res.render('index', { title: 'Chorizo Burrito', tweets: links, pictures: pics });
+
+      // })
+
+
+
+    })
 
 
 });
+
 
 module.exports = router;
