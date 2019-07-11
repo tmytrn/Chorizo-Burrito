1986031c673f813e0be7e41e339b3787d8bdb7e7
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2019-07-10 17:26:15 -0700

    added about page

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about');
+  res.render('about', {title: 'Chorizo Burrito'});
 });
 
 module.exports = router;
