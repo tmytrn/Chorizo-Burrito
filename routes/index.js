5408d795085aa2fe11e74dc51b89fc5fd675e501
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2020-06-05 02:42:00 -0700

    about page changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let Twitter = require('twitter');
-var request = require('request-promise');
+const fetch = require('node-fetch');
 const redis = require('redis');
-
+var SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config();
 
 const REDIS_PORT = process.env.REDIS_URL || 6379;
@@ -17,7 +17,44 @@ var client = new Twitter({
   access_token_secret: process.env.TWIT_ACCESS_SECRET
 });
 
+let spotifyApi = new SpotifyWebApi({
+  clientId: '0d0814d91f8a4b61b09fb602ed6dea6b',
+  clientSecret: 'f9d22b486ce544feb3be2242cd1939a7'
+});
+
+// spotifyApi.clientCredentialsGrant().then(
+//   function(data) {
+//     console.log('The access token expires in ' + data.body['expires_in']);
+//     console.log('The access token is ' + data.body['access_token']);
+
+//     // Save the access token so that it's used in future calls
+//     spotifyApi.setAccessToken(data.body['access_token']);
+//   },
+//   function(err) {
+//     console.log('Something went wrong when retrieving an access token', err);
+//   }
+// );
+
 let links, pics;
+
+/* GET home page. */
+router.get('/', cache, getTweets);
+
+//  async function getPlaylist(req, res, next){
+//   spotifyApi.getPlaylist('53SJ6SSjNYgJQftjhBk3Er').then(
+//     function(data) {
+//       // console.log(data.body.tracks.items);
+//       let tracks = data.body.tracks.items;
+//       for(i =0; i < tracks.length; i++){
+//         console.log(tracks[i].track.name);
+//       }
+//       next();
+//     },
+//     function(err) {
+//       console.error(err);
+//     }
+//   );
+//   }
 
 async function getTweets(req, res, next){
   try{
@@ -26,9 +63,9 @@ async function getTweets(req, res, next){
     const params = { exclude_replies: true, count: 200 }
     const response = await client.get('statuses/home_timeline', params);
     // const data = await response.json();
-    tweets = cleanTweets(response);
+    tweets =  cleanTweets(response);
 
-    cachePort.setex('home', 3600, JSON.stringify(tweets));
+    cachePort.setex('home', 100, JSON.stringify(tweets));
 
     links = tweets.links;
     pics = tweets.pics;
@@ -46,7 +83,7 @@ function cache(req, res, next) {
 
     if (data !== null) {
       data = JSON.parse(data);
-      console.log(data);
+      // console.log(data);
       links = data.links;
       pics = data.pics;
       res.render('index', { title: 'Chorizo Burrito', tweets: links, pictures: pics });
@@ -56,12 +93,7 @@ function cache(req, res, next) {
   });
 }
 
-
-
-/* GET home page. */
-router.get('/',cache, getTweets);
-
-const cleanTweets = (tweets) => {
+ function cleanTweets(tweets) {
   let obj = {};
   obj.links = [];
   obj.pics = [];
@@ -75,7 +107,7 @@ const cleanTweets = (tweets) => {
         obj.pics.push(picturePost(tweets[i], cleanText))
       }
       else {
-        obj.links.push(linkPost(tweets[i], cleanText))
+        obj.links.push( linkPost(tweets[i], cleanText))
       }
 
     }
@@ -102,7 +134,7 @@ const picturePost = (tweet, cleanText) => {
   return element;
 }
 
-const linkPost = (tweet, cleanText) => {
+ function linkPost(tweet, cleanText) {
   if (!tweet.entities.urls.length == 0) {
     // console.log(tweet.user.screen_name);
     let url = tweet.entities.urls[0].url;
@@ -110,43 +142,46 @@ const linkPost = (tweet, cleanText) => {
     let source = tweet.id_str;
     let screenName = tweet.user.screen_name;
     let sourceLink = 'https://twitter.com/' + screenName + '/status/' + source;
+    let display_url = tweet.entities.urls[0].display_url;
+
 
     let element = {
       source: sourceLink,
       url: url,
       text: cleanText,
-      author: author
+      author: author,
+      display_url: display_url
     }
     return element;
 
   }
 }
 
-const lookUp = (url) => {
+  async function lookUp(url){
   const lookUpKey = process.env.LINK_PREVIEW_KEY;
   const site = 'https://api.linkpreview.net';
   const apiEndPoint = site + '/?key=' + lookUpKey + '&q=' + url;
+  let imageURL;
 
   let options = {
-    method: "GET",
-    uri: apiEndPoint,
-    json: true,
     headers: {
       q: apiEndPoint,
       key: lookUpKey,
     }
   }
 
-  return new request(options)
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      console.log(error);
+  const response =  await fetch(apiEndPoint, options);
 
-    })
+  const data =  await response.json(); 
+  console.log(data.description);
+  const image = data.image;
+  // console.log(image);
+
+  return image;
 
 }
+
+
 
 
 module.exports = router;
