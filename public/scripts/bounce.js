1c799affd4bf7f24169a2d4f42d45520cc9a26af
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2020-12-04 00:22:12 -0800

    last fixes

--- public/scripts/bounce.js
+++ public/scripts/bounce.js
@@ -3,12 +3,12 @@ var x = 100;
 var y = 200;
 var x1 = 200;
 var y1 = 400;
-var x2 = 150;
-var y2 = 400;
+var x2 = 300;
+var y2 = 200;
 var x3 = 200;
 var y3 = 0;
 var x4 = 0;
-var y4 = 600;
+var y4 = 500;
 var dx = -3;
 var dy = 3;
 var dx1 = 1.75;
@@ -45,47 +45,45 @@ function init() {
 }
 
 function draw() {
-  if ($("#Something").isInViewport()) {
-    context.canvas.width = window.innerWidth;
-    context.canvas.height = window.innerHeight;
-    canvas = document.getElementById("myCanvas");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.fillStyle = "#032dff";
-    var imageSize = 100;
-    if (canvas.width < 380) {
-      imageSize = 75;
-    }
-    // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-    context.drawImage(img, x, y, imageSize, imageSize);
-    context.drawImage(img1, x1, y1, imageSize, imageSize);
-    context.drawImage(img2, x2, y2, imageSize, imageSize);
-    context.drawImage(img3, x3, y3, imageSize, imageSize);
-    context.drawImage(img4, x4, y4, imageSize, imageSize);
-    context.closePath();
-    context.fill();
-    // Boundary Logic
-    if (x < 0 || x > window.innerWidth - imageSize) dx = -dx;
-    if (y < 0 || y > window.innerHeight - imageSize) dy = -dy;
-    if (x1 < 0 || x1 > window.innerWidth - imageSize) dx1 = -dx1;
-    if (y1 < 0 || y1 > window.innerHeight - imageSize) dy1 = -dy1;
-    if (x2 < 0 || x2 > window.innerWidth - imageSize) dx2 = -dx2;
-    if (y2 < 0 || y2 > window.innerHeight - imageSize) dy2 = -dy2;
-    if (x3 < 0 || x3 > window.innerWidth - imageSize) dx3 = -dx3;
-    if (y3 < 0 || y3 > window.innerHeight - imageSize) dy3 = -dy3;
-    if (x4 < 0 || x4 > window.innerWidth - imageSize) dx4 = -dx4;
-    if (y4 < 0 || y4 > window.innerHeight - imageSize) dy4 = -dy4;
-    x += dx;
-    y += dy;
-    x1 += dx1;
-    y1 += dy1;
-    x2 += dx2;
-    y2 += dy2;
-    x3 += dx3;
-    y3 += dy3;
-    x4 += dx4;
-    y4 += dy4;
+  context.canvas.width = window.innerWidth;
+  context.canvas.height = window.innerHeight;
+  canvas = document.getElementById("myCanvas");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.beginPath();
+  context.fillStyle = "#032dff";
+  var imageSize = 100;
+  if (canvas.width < 380) {
+    imageSize = 75;
   }
+  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  context.drawImage(img, x, y, imageSize, imageSize);
+  context.drawImage(img1, x1, y1, imageSize, imageSize);
+  context.drawImage(img2, x2, y2, imageSize, imageSize);
+  context.drawImage(img3, x3, y3, imageSize, imageSize);
+  context.drawImage(img4, x4, y4, imageSize, imageSize);
+  context.closePath();
+  context.fill();
+  // Boundary Logic
+  if (x < 0 || x > window.innerWidth - imageSize) dx = -dx;
+  if (y < 0 || y > window.innerHeight - imageSize) dy = -dy;
+  if (x1 < 0 || x1 > window.innerWidth - imageSize) dx1 = -dx1;
+  if (y1 < 0 || y1 > window.innerHeight - imageSize) dy1 = -dy1;
+  if (x2 < 0 || x2 > window.innerWidth - imageSize) dx2 = -dx2;
+  if (y2 < 0 || y2 > window.innerHeight - imageSize) dy2 = -dy2;
+  if (x3 < 0 || x3 > window.innerWidth - imageSize) dx3 = -dx3;
+  if (y3 < 0 || y3 > window.innerHeight - imageSize) dy3 = -dy3;
+  if (x4 < 0 || x4 > window.innerWidth - imageSize) dx4 = -dx4;
+  if (y4 < 0 || y4 > window.innerHeight - imageSize) dy4 = -dy4;
+  x += dx;
+  y += dy;
+  x1 += dx1;
+  y1 += dy1;
+  x2 += dx2;
+  y2 += dy2;
+  x3 += dx3;
+  y3 += dy3;
+  x4 += dx4;
+  y4 += dy4;
 }
 init();
 window.addEventListener("resize", function () {
